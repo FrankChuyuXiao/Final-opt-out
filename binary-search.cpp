753d9469faeefe1ce888b93289fea01befe35892
Chuyu Xiao <chuyu_xiao@ucsb.edu>
2024-12-07 19:56:43 -0800

    second

--- binary-search.cpp
+++ binary-search.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
 using namespace std;
 
 //Define the node as a struct
@@ -10,9 +10,9 @@ struct node {
     node(int x) : val(x), left(nullptr), right(nullptr) {}
 };
 
-//Recursive insert function
+//Recursive function for inserting a key into BST tree
 node* insert(node* root, int key) {
-    if (!root) return new node(key); //for root = null.
+    if (root == nullptr) return new node(key); //for root = null.
     if (key < root->val) {
         root->left = insert(root->left, key);
     } else {
@@ -21,30 +21,32 @@ node* insert(node* root, int key) {
     return root;
 }
 
-//Preorder traversal of the BST
+//Preorder traversal
 void preorder(node* root) {
-    if (!root) return;
+    if (!root)
+        return;
     cout << root->val << " ";
     preorder(root->left);
     preorder(root->right);
 }
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int n;
     cin >> n;
-    vector<int> a(n);
+    int x[n];
+    node* root = nullptr;
 
     // Read the input sequence
     for (int i = 0; i < n; ++i) {
-        cin >> a[i];
+        cin >> x[i];
     }
 
-    // Construct the BST
-    node* root = nullptr;
     for (int i = 0; i < n; ++i) {
-        root = insert(root, a[i]);
+       root = insert(root, x[i]);
     }
-
     // Output the preorder traversal
     preorder(root);
     cout << endl;
